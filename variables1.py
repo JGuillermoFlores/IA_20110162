c7d01c9b07411db1fc28986d3a3ce6c5f53a237d
JGuillermoFlores <112290860+JGuillermoFlores@users.noreply.github.com>
2022-08-27 22:05:11 -0500

    P001

---
+++ variables1.py
@@ -0,0 +1,5 @@
+m="Bienvenido al programa de Variables"      #Asignamos a la variable m el string
+print(m)                                     #Imprimimos en la consola el valor almacenado en la variable m 
+x = 12;y =15                                 #Creamos las variables x y y les damos valores numericos 
+z = x+y                                      #Vamos a hacer una suma con las variables x y que se almacenara en la variable
+print ("El resultado de la suma es",z)       #Impirmir un string que nos mencione que es el resultado, seguido de la variable del resultado
