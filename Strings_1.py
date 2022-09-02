e490b0156e3d42bc893180b106e7a349132f0c3d
JGuillermoFlores <112290860+JGuillermoFlores@users.noreply.github.com>
2022-09-02 11:19:13 -0500

    P002

---
+++ Strings_1.py
@@ -0,0 +1,6 @@
+mensaje = "Bienvenido al capitulo de Strings 1"
+print(mensaje)
+mensaje = '\n\nBienvenido al capitulo de Strings 1'
+print(mensaje)
+mensaje ='\n\n"print()" se utiliza para imprimir valores en la consola.'
+print(mensaje)
