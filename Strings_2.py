d1cb87bbbf9c3fc298692ec687848954024ed2c1
JGuillermoFlores <112290860+JGuillermoFlores@users.noreply.github.com>
2022-09-02 12:31:50 -0500

    P003

---
+++ Strings_2.py
@@ -0,0 +1,23 @@
+mensaje = "Asi se concatenan " + "dos o mas strings en una variable"
+print(mensaje)
+
+mensaje ="\n\nViernes 2 de "
+mensaje1 ="septiembre de 2022\n\n"
+mensaje2 =mensaje + mensaje1
+print(mensaje2)
+
+
+print(mensaje + "septiembre de 2022")
+N1 = "\n\nJose "
+N2 = "Guillermo "
+A1 = "Flores "
+A2 = "Castañeda"
+
+nc = N1+N2+A1+A2
+print(nc)
+
+N1="20"
+N2="00"
+
+nc=N1+N2
+print(nc)
