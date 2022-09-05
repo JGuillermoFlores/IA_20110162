8ba22cd445b9a40f68a9a3f0c83dd5895ef97bda
JGuillermoFlores <112290860+JGuillermoFlores@users.noreply.github.com>
2022-09-04 22:40:28 -0500

    P006

---
+++ Operadores_1.py
@@ -0,0 +1,5 @@
+suma = 20 + 21 + 22 + 24
+resta = 0 - 20 - 21 - 22 - 24
+multiplicacion = 29 * 3
+division = 5000 / 5 / 100
+print("Suma: " , suma ,  "\nResta: " , resta , "\nMultiplicación: " , multiplicacion , "\nDivisión: " , division)
