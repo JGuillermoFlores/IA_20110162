4c479d1586ee10ef0091054ebd790f99c394dfe8
JGuillermoFlores <112290860+JGuillermoFlores@users.noreply.github.com>
2022-09-05 23:40:27 -0500

    P024

---
+++ Funciones_python.py
@@ -0,0 +1,12 @@
+#Crea una función que realice una suma. Para ello, tendrás que añadir dos argumentos (numero1 y numero2).
+# En su interior, especificarás un print() que muestre el resultado de la suma. Deberás hacer tres llamadas
+#  que como resultado de la suma den los valores 30, 50 y 57000. Los números que introduzcas en la llamada
+#   pueden ser los que quieras siempre que coincidan los resultados en el print().
+
+
+def suma(numero1, numero2):   #Creamos la funcion con 'def' y establecemos ambos numeros
+	print(numero1 + numero2)  #Imprimimos la suma de ambos numeros 
+
+suma(5, 25)
+suma(25, 25)                     #Le damos valores a la funcion 
+suma(50000, 7000)
