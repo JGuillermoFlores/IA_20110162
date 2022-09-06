b2b49928f0257776ee58402a705a7634434f53ae
JGuillermoFlores <112290860+JGuillermoFlores@users.noreply.github.com>
2022-09-05 23:16:41 -0500

    P013

---
+++ If_condicional.py
@@ -0,0 +1,21 @@
+#Cambia el operador para que la condición sea True.
+
+num1 = 15        #Se declaran ambas variables 
+num2 = 20
+
+if num1 < num2:   #Corregimos el operador donde se dice que el 15 es menor que el 20
+	print('Se ejecuta el if.')  #Se imprime en sonsola porque es verdadera
+
+#Cambia el operador para que la condición sea True
+num1 = 1450     #Se declaran las variables
+num2 = 60
+
+if num1 > num2:   #Decimos que 1450 es mayor que 60
+	print('Se ejecuta el if.') #Se imprime en consola porque es True
+
+#Haz que el siguiente condicional se convierta en False sin cambiar el operador
+num1 = 60           #Se declaran ambas variables iguales para que no se cumpla la condicion
+num2 = 60
+
+if num1 != num2:  #Decimos que 60 es diferente a 60
+	print('Se ejecuta el if.') #No se imprime en consola porque no se cumple la condicion es False
