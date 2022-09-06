f69052ca06db5e10042e961f9a3b64aa6452770e
JGuillermoFlores <112290860+JGuillermoFlores@users.noreply.github.com>
2022-09-05 23:27:01 -0500

    P021

---
+++ Diccionarios.py
@@ -0,0 +1,14 @@
+#Del diccionario teclado2 del capítulo, muestra los elementos Modelo y Precio con presentación en un print(). El resultado será esto en la consola:
+teclado1 = {
+	'Categoría': 'Teclados',
+	'Modelo': 'HyperX Alloy FPS Pro',    #Creamos el teclado 1 , los diccionarios se crean con llaves
+	'Precio': '89,99'
+}
+
+teclado2 = {
+	'Categoría': 'Teclados',
+	'Modelo': 'Corsair K55 RGB',         #Declaramos el diccionario dos con sus elementos 
+	'Precio': '59,99'
+}
+
+print('El modelo', teclado2['Modelo'], 'cuesta', teclado2['Precio'], '$.')  #Imprimimos sus elementos del teclado 2 
