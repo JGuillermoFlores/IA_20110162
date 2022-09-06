7e80cb29607c3fb825e2a2e238d64c421faf5091
JGuillermoFlores <112290860+JGuillermoFlores@users.noreply.github.com>
2022-09-05 23:26:28 -0500

    P020

---
+++ Range_For.py
@@ -0,0 +1,3 @@
+#Crea un bucle for con un range() que vaya desde el valor 7 hasta el valor 700 en saltos de 100. Basta con que imprimas el valor de cada iteración.
+for x in range(7, 700, 100): #Establecemos un for donde el rango empieza en 7 hasta 700 y aumenta de 100 en 100
+	print(x)  #Se imprime el for 
