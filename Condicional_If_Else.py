958aaca02f88f189e2260140414e6ec58f68a864
JGuillermoFlores <112290860+JGuillermoFlores@users.noreply.github.com>
2022-09-05 23:17:28 -0500

    P014

---
+++ Condicional_If_Else.py
@@ -0,0 +1,16 @@
+#Corrige el siguiente condicional if else.
+ #color = rojo
+
+#else color == rojo
+#Print "El color es rojo."
+#if color != rojo
+#Print "El color no es rojo."
+
+color='rojo'    #Declaramos el color con comillas
+if color != 'rojo': #Establecemos primero el if antes del else , rojo en comillas y con dos puntos al final  
+    print("El color no es rojo ") #print se declara con minusculas 
+else: #En el else ya no se establece otra condicion 
+    print("El color es rojo") #print nuevamente en minusculas y su contenido entre parentesis
+ 
+
+
